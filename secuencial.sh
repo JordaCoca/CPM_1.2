050d76bd8a794ff9ce39173a2f8703644c2f3c5e
Jordà Coca <jordacrodriguez02@gmail.com>
2026-03-22 20:32:25 +0100

    a ver secuencial º__º

---
+++ secuencial.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SRC="./mD8K_comentado.c"
+RESULTS_DIR="./3_resultados_md8k"
+BIN="./md8k_pal2"
+
+echo "Compilando programa..."
+cc -O3 -fopenmp $SRC -o $BIN
+
+if [ $? -ne 0 ]; then
+    echo "Error en compilacion"
+    exit 1
+fi
+mkdir -p $RESULTS_DIR
+
+echo "===== EJECUCIONES EN ORCA SECUENCIAL ====="
+export OMP_NUM_THREADS=1
+srun -p orca time $BIN &> $RESULTS_DIR/SECUENCIAL
+
+echo "Ejecucion completa"
